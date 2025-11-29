4ce18a3eff383eb1ededef2e471a8bbda77a0187
Eduardo Elizondo <eduardo.elizondof@gmail.com>
2025-11-28 23:43:22 -0600

    cambios al welcome

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="robots" content="noindex, nofollow">
 
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
         <script>
@@ -30,11 +31,13 @@
             }
         </style>
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>FECME MX Federación Canina de México</title>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" href="/favicon/favicon.ico" sizes="any">
+        <link rel="icon" href="/favicon/favicon.svg" type="image/svg+xml">
+        <link rel="icon" href="/favicon/favicon-96x96.png" sizes="96x96" type="image/png">
+        <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png">
+        <link rel="manifest" href="/favicon/site.webmanifest">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
